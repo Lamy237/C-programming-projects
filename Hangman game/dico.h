26a41cdbe1c179b982b352b2ae795b7a1ce7101f
Lamy237 <89041260+Lamy237@users.noreply.github.com>
2022-07-04 04:01:14 +0530

    Update dico.h
    Correct the date of the last update

--- Hangman game/dico.h
+++ Hangman game/dico.h
@@ -7,7 +7,7 @@ By Lamy237, for the HANGMAN game (OpenClassrooms)
 Role: *Prototypes of the functions used to randomly choose a word from the dictionary (MODE 1)
       *Definition of the constants used in the program (Preprocessor)
 
-Last Update: June 12, 2022
+Last Update: July 4, 2022
 */
 
 #ifndef DICO_H_INCLUDED
