00b1090da7f2ee99759063471a0285592651f5a6
Lamy237 <89041260+Lamy237@users.noreply.github.com>
2022-07-04 03:55:30 +0530

    Update functions.c

--- Hangman game/functions.c
+++ Hangman game/functions.c
@@ -6,12 +6,9 @@ By Lamy237, for the HANGMAN game (OpenClassrooms)
 
 Role: Definition of the functions managing the game
 
-      - read_char() : Reads only the first character entered and erases all the others (empty the buffer)
       - search_in_word() : Verifies whether or not the entered letter is in the word
       - display_word() : Prints the secret word while masking the letters that haven't been found yet
       - win() : Checks whether all the letters of the word were found or not
-      - read_string() : Reads the secret word entered by the user and frees the buffer (MODE 2 only)
-      - read_long() : Converts a string into a number (long int)
       - hang() : Draws a man getting hanged more and more each time the player enters a wrong letter
 
 Creation: June 30, 2021 at 15:29 (Indian Time)
@@ -21,23 +18,8 @@ Last Update: June 12, 2022
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
 
-#include "dico.h" //To use the preprocessor MAX
-
-
-char read_char()
-{
-    char myChar = 0;
-
-    myChar = getchar();
-    myChar = toupper(myChar); //Puts the entered character in upper case if not already in
-
-    while(getchar() != '\n'); //Read all the remaining characters to free the buffer
-
-    return myChar;
-}
+#include "functions.h"
 
 
 int search_in_word(const char letter, const char word[], int table[])
@@ -80,59 +62,6 @@ int win(const int table[], int size)
     }
 
     return success;
-}
-
-
-void free_buffer()
-{
-    int c = 0;
-
-    while(c != '\n' && c != EOF)
-    {
-        c = getchar();
-    }
-}
-
-
-int read_string(char myString[])
-{
-    char *position = NULL;
-
-    if(fgets(myString, MAX, stdin) != NULL)
-    {
-        position = strchr(myString, '\n');
-
-        if(position != NULL)
-        {
-            *position = '\0';
-        }
-        else
-        {
-            free_buffer();
-        }
-
-        return 1;
-    }
-    else
-    {
-        free_buffer();
-        return 0;
-    }
-}
-
-
-int read_long()
-{
-    char numberText[MAX] = {0};
-
-    if(read_string(numberText))
-    {
-        return strtol(numberText, NULL, 10);
-    }
-    else
-    {
-        return 0;
-    }
 }
 
 
